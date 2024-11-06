ca4fd63f0346916166d8ce602df16b7e34484860
NickStassen <nicholas@stassen.com>
2024-11-06 12:00:09 -0500

    Creates new system for working on the frontend with the backend api dynamically. Npm serve-like functionality using the django webserver

--- vue-frontend/vue.config.js
+++ vue-frontend/vue.config.js
@@ -1,4 +1,6 @@
-const { defineConfig } = require('@vue/cli-service')
+const { defineConfig } = require('@vue/cli-service');
+const webpack = require('webpack');
+
 module.exports = defineConfig({
   transpileDependencies: true,
 
@@ -8,16 +10,31 @@ module.exports = defineConfig({
   configureWebpack: {
     resolve: {
       fallback: { https: false, zlib: false, http: false, url: false },
+    },
+    plugins: [
+      new webpack.DefinePlugin({
+        __VUE_PROD_HYDRATION_MISMATCH_DETAILS__: JSON.stringify(false),
+      }),
+      new webpack.ProvidePlugin({
+        adapter: ['webrtc-adapter', 'default']
+      })
+    ],
+    module: {
+      rules: [
+        {
+          test: require.resolve('janus-gateway'),
+          loader: 'exports-loader',
+          options: {
+            exports: 'Janus',
+          },
+        }
+      ]
     }
-  },
-
-  devServer: {
-    historyApiFallback: true,
   },
 
   pluginOptions: {
     vuetify: {
-      // https://github.com/vuetifyjs/vuetify-loader/tree/next/packages/vuetify-loader
+      // Options for Vuetify loader (if any)
     }
   }
 })
