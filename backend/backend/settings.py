a4a959a02986373dc41582a8d06f1ef9d11026f2
TeaBear5 <tyler.bair@protonmail.com>
2024-11-25 11:38:42 -0500

    Adds all hosts with *

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-=tac9fw81&4hk9pm(ypzq!+b)^d#du$452n#a5ezljuev&hhy-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
